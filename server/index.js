26f22597d5d57db3bcf46654d491e18add393741
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-08-27 14:41:05 +0200

    crud for posts is go, c for comments also

--- server/index.js
+++ server/index.js
@@ -53,24 +53,69 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api', verifyToken, (req, res) => {
-  
-    res.json({ message: 'Hello API!' });
-  
+
+// routes for crud operations on posts.
+
+app.get('/api/posts/:postid', verifyToken, async (req, res) => {
+    const postId = parseInt(req.params.postid);
+    try {
+    const post = await database.getPostById(postId);
+    console.log(post);
+    res.json({ post: post });
+    } catch (error) {
+      console.error('Error fetching post from server: ',error);
+      return res.status(500).json({error:'Error fetching post.'});
+    }
 });
 
 app.post('/api/posts', verifyToken, verifyAdmin, async (req, res) => {
     try {
        const post = await database.createPost(req.body.title, req.body.text, req.token.id);
-       console.log('Post inserted correctly in database: ',post),
+       
        res.json({ message: 'Post inserted correctly in database'});       
     } catch (error) {
         console.error('Error creating post', error);
-        return res.status(500).send('Error creating post, please try again.');
+        return res.status(500).json({error:'Error creating post.'});
     }
   
 });
 
+app.patch('/api/posts/:postid', verifyToken, verifyAdmin, async (req, res) => {
+    const postId = parseInt(req.params.postid);
+    const updateData = parseEditPostBody(req.body);
+  try {
+    const updatedPost = await database.updatePost(postId, updateData);
+    res.json( {message: 'Post updated successfully'});
+    
+  } catch (error) {
+    console.error('Error updating post: ', error);
+    return res.status(500).json({error:'Error updating post.'})
+  }
+})
+
+app.delete('/api/posts/:postid', verifyToken, verifyAdmin, async (req, res) => {
+  const postId = parseInt(req.params.postid);
+  try {
+    const post = await database.deletePostById(postId);
+    res.json( { message: 'Post deleted correctly from database.'})
+  } catch (error) {
+    console.error('Error deleting post from database: ',error);
+    return res.status(500).json({error:'Error deleting post.'});
+  }
+});
+
+
+app.post('/api/posts/:postid/comments', verifyToken, async (req, res) => {
+  const postId = parseInt(req.params.postid);
+  const userId = req.token.id;
+  try {
+    const comment = await database.createComment(userId, req.body.text, postId);
+    res.json( { message: 'Comment created successfully', comment: comment});
+  } catch (error) {
+    console.error('Error creating comment',error);
+    return res.status(500).json({error:'Error creating comment'});
+  }
+});
 
 
 app.post('/login', (req, res, next) => {
@@ -101,7 +146,7 @@ app.post('/register', async (req, res) => {
     res.redirect('/register');
   } catch (error) {
     console.error('Error creating account', error);
-    return res.status(500).send('Error creating account, please try again.');
+    return res.status(500).json({errorMsg:'Error creating account.', error});
   }
 });
 
@@ -129,10 +174,29 @@ function verifyToken(req, res, next) {
 }
 
 function verifyAdmin(req, res, next) {
-    if (req.user.type !== "Admin") {
+    if (req.token.type !== "Admin") {
         return res.sendStatus(403);
     }
     next();
+}
+
+// Utility function 
+
+function parseEditPostBody(formBody) {
+  const updateData = {};
+    if (formBody.title) {
+      updateData.title = formBody.title;
+    }
+    if (formBody.text) {
+      updateData.text = formBody.text;
+    }
+    if (formBody.isPublished) {
+      const isPub = formBody.isPublished == 'true' ? true : false;
+      updateData.isPublished = isPub;
+      updateData.createdAt = new Date();
+    }
+
+    return updateData;
 }
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
--- server/src/services/database.js
+++ server/src/services/database.js
@@ -43,6 +43,18 @@ async function createUser(formBody, hashedPassword) {
     
 }
 
+async function getPostById(postId) {
+    try {
+        const post = await prisma.post.findUnique({
+            where: {id: postId},
+        });
+        return post;
+    } catch (error) {
+        console.error(`Error fetching post id:'${postId}' from database: `, error);
+        throw error;
+    }
+}
+
 async function createPost(title, text, authorId) {
     try {
         const post = await prisma.post.create({
@@ -52,6 +64,7 @@ async function createPost(title, text, authorId) {
                 authorId: authorId,
             },
         });
+        console.log('Post inserted correctly in database: ',post);
         return post;
     } catch (error) {
         console.error('Error creating post: ', error);
@@ -59,5 +72,51 @@ async function createPost(title, text, authorId) {
     }
 }
 
+async function updatePost(postId, data) {
+    try {
+        const post = await prisma.post.update({
+            where: {id: postId},
+            data: data,
+        });
+        console.log('Post successfully updated: ',post);
+        return post;
+    } catch {
+        console.error('Error updating post: ',error);
+        throw error;
+    }
+}
 
-module.exports = { getUserByName, getUserById, createUser, createPost };
+async function deletePostById(postId) {
+    try {
+        const post = await prisma.post.delete({
+            where:{
+                id:postId
+            }});
+            console.log('Post deleted correctly from database: ',post);
+            return post; 
+    } catch (error) {
+        console.error('Error deleting post: ', error);
+        throw error;
+    }
+
+}
+
+async function createComment(commenterId, text, parentId) {
+    try {
+        const comment = await prisma.comment.create({
+            data: {
+                text: text,
+                commenterId: commenterId,
+                parentId: parentId,
+            },
+        });
+        console.log('Comment added successfully', comment);
+        return comment;
+    } catch (error) {
+        console.error('Error creating comment',error);
+        throw error;
+    }
+}
+
+
+module.exports = { getUserByName, getUserById, createUser, getPostById, createPost, updatePost, deletePostById, createComment };
